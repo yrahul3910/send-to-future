b04547aa291f54bcd2a3b14a9b89d940f6e1df45
Rahul Yedida <yrahul3910@gmail.com>
2021-02-26 14:10:35 -0500

    Initial Commit

---
+++ SendToFuture/SendToFutureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendToFutureApp.swift
+//  SendToFuture
+//
+//  Created by Rahul Yedida on 2/26/21.
+//
+
+import SwiftUI
+
+@main
+struct SendToFutureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
