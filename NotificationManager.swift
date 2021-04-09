be38489f1315108ae022d88edf00d636bde2772b
Rahul Yedida <yrahul3910@gmail.com>
2021-04-09 02:42:18 -0400

    Fix #2

--- NotificationManager.swift
+++ NotificationManager.swift
@@ -21,6 +21,51 @@ class NotificationManager: NSObject, ObservableObject {
             }
         }
     }
+    
+    func scheduleNotification(url: String, title: String, time: Int = 3600) {
+        // Define the custom actions.
+        let acceptAction = UNNotificationAction(identifier: "ACCEPT_ACTION",
+                                                title: "Open",
+                                                options: UNNotificationActionOptions(rawValue: 0))
+        let deferAction = UNNotificationAction(identifier: "DEFER_1HOUR_ACTION",
+                                               title: "Remind me in 1 hour",
+                                               options: UNNotificationActionOptions(rawValue: 0))
+        
+        // Define the notification type
+        let notifCategory =
+            UNNotificationCategory(identifier: "FUTURE_NOTIFICATION",
+                                   actions: [acceptAction, deferAction],
+                                   intentIdentifiers: [],
+                                   hiddenPreviewsBodyPlaceholder: "",
+                                   options: .customDismissAction)
+        
+        // And now schedule the notification
+        let content = UNMutableNotificationContent()
+        content.title = "The future is now!"
+        content.body = "Tap to open the link you sent to future you."
+        content.userInfo = ["TITLE": title,
+                            "URL": url]
+        content.categoryIdentifier = "FUTURE_NOTIFICATION"
+        
+        // Create notification trigger
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval.init(time), repeats: false)
+        
+        // Create notification request
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString,
+                                            content: content, trigger: trigger)
+        
+        // Schedule the request with the system.
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.setNotificationCategories([notifCategory])
+        notificationCenter.add(request) { (error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            } else {
+                print("Notification sent successfully")
+            }
+        }
+    }
 }
 
 extension NotificationManager: UNUserNotificationCenterDelegate {
@@ -36,6 +81,7 @@ extension NotificationManager: UNUserNotificationCenterDelegate {
             "FUTURE_NOTIFICATION" {
             // Retrieve the details.
             let pageURL = userInfo["URL"] as! String
+            let title = userInfo["TITLE"] as! String
             
             switch response.actionIdentifier {
             case "ACCEPT_ACTION", UNNotificationDefaultActionIdentifier:
@@ -47,6 +93,7 @@ extension NotificationManager: UNUserNotificationCenterDelegate {
                 break
                 
             case "DEFER_1HOUR_ACTION":
+                self.scheduleNotification(url: pageURL, title: title)
                 break
                 
             case UNNotificationDismissActionIdentifier:
